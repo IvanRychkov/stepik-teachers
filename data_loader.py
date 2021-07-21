1da0b97639f5620cd476260b914264dac97975a0
Ivan Rychkov <rychkov.ivan@gmail.com>
2021-07-22 02:25:19 +0300

    feat: add data_loader.py, loading data on import

---
+++ data_loader.py
@@ -0,0 +1,16 @@
+import json
+from data import data
+
+print('data_loader run')
+# Сохраняем все данные в один словарь
+all_data = dict(goals=data.goals, teachers=data.teachers)
+
+# Создаём файл JSON
+file_path = './data/data.json'
+with open(file_path, 'w') as f:
+    json.dump(all_data, f, ensure_ascii=False)
+
+print('data loaded into', file_path)
+
+if __name__ == '__main__':
+    print(all_data)
