8c56eeb2e015ec6111010cd1302da337b242a88f
Ivan Rychkov <rychkov.ivan@gmail.com>
2021-07-22 23:02:22 +0300

    feat: add html rendering to all routes

--- app.py
+++ app.py
@@ -8,46 +8,50 @@ app = Flask(__name__)
 @app.route('/')
 def render_main():
     """Здесь будет главная"""
-    pass
+    return render_template('index.html')
 
 
 @app.route('/all/')
 def render_all():
     """здесь будут преподаватели"""
-    pass
+    return render_template('all.html')
 
 
 @app.route('/goals/<goal>/')
 def render_goal(goal):
     """здесь будет цель"""
-    pass
+    render_template('goal.html')
 
 
 @app.route('/profiles/<int:teacher_id>')
 def render_profile(teacher_id):
     """здесь будет преподаватель"""
-    pass
+    return render_template('profile.html')
 
 
 @app.route('/request/')
 def render_request_form():
     """Заявка на подбор"""
-    pass
+    return render_template('request.html')
 
 
 @app.route('/request_done/')
 def render_request_done():
     """заявка на подбор отправлена"""
-    pass
+    return render_template('request_done.html')
 
 
 @app.route('/booking/<int:teacher_id>/<int:weekday>/<time>/')
 def render_booking_form(teacher_id, weekday, time):
     """здесь будет форма бронирования"""
-    pass
+    return render_template('booking.html')
 
 
 @app.route('/booking_done/')
 def render_booking_done():
     """заявка отправлена"""
-    pass
+    return render_template('booking_done.html')
+
+
+if __name__ == '__main__':
+    app.run()
